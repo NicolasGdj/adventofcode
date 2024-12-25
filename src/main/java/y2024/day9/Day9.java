86813f2f047a18f423d9c66132b0063e99c97222
NicolasGdj <nicolas-guerroudj@hotmail.com>
2024-12-25 13:47:46 +0100

    feat(Day9): part2

--- src/main/java/y2024/day9/Day9.java
+++ src/main/java/y2024/day9/Day9.java
@@ -56,5 +56,60 @@ public class Day9 extends Day {
 
     @Override
     public void resolveTwo(List<String> lines) {
+        String input = lines.get(0);
+        boolean block = true;
+        List<Integer> ids = new ArrayList<>();
+        char id = 0;
+        for(String character : input.split("")) {
+            int count = Integer.parseInt(character);
+            for (int i = 0; i < count; i++) {
+                ids.add(block ? id :  -1);
+            }
+            block = !block;
+            if(block) {
+                ++id;
+            }
+        }
+        System.out.println(ids);
+        for(int j = ids.size() - 1; j >= 0; --j) {
+            if(ids.get(j) != -1) {
+                int blockSize = 0;
+                for(int k = j; k >= 0; --k) {
+                    if(ids.get(k).equals(ids.get(j))) {
+                        blockSize++;
+                    } else {
+                        break;
+                    }
+                }
+
+                for(int i = 0; i < j; ++i) {
+                    if(ids.get(i) == -1) {
+                        int freeSpace = 1;
+                        for(int k = i+1; k < ids.size(); ++k) {
+                            if(ids.get(k) == -1) {
+                                ++freeSpace;
+                            } else {
+                                break;
+                            }
+                        }
+                        if(freeSpace >= blockSize) {
+                            for(int k = 0; k < blockSize; ++k) {
+                                Collections.swap(ids, i + k , j - k);
+                            }
+                        }
+                    }
+                }
+                j -= blockSize - 1;
+            }
+        }
+        System.out.println(ids);
+
+        long total = 0;
+        for(int i = 0; i < ids.size(); ++i) {
+            if(ids.get(i) > 0) {
+                total += ids.get(i) * i;
+            }
+        }
+        System.out.println(total);
     }
 }
